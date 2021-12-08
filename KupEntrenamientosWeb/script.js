da7437b8927b576b7df714c73c0e74d164a75935
Kup87 <dami.kuper@gmail.com>
2021-12-08 18:49:19 -0300

    si

--- KupEntrenamientosWeb/script.js
+++ KupEntrenamientosWeb/script.js
@@ -1,3 +1,24 @@
+//navbar
+const mainNavigation = document.querySelector(".main-navigation");
+const overlay = document.querySelector(".overlay");
+const toggler = document.querySelector(".navbar-toggler");
+
+const openSideNav = () => {
+    mainNavigation.classList.add("active");
+    overlay.style.display = "flex";
+}
+const closeSideNav = () => {
+    mainNavigation.classList.remove("active");
+    overlay.style.display = "none";
+}
+
+document.addEventListener("swiped-left", openSideNav);
+document.addEventListener("swiped-right", closeSideNav);
+toggler.addEventListener("click", openSideNav);
+overlay.addEventListener("click", closeSideNav);
+
+
+
 //Activa on scroll view la animación "GaleriaRtoL"
 window.addEventListener('scroll', function(){
     let gal_img = document.getElementsByClassName("gal_img");
