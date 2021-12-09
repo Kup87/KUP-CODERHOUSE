c463b473d15e7095e613c8bf04f6085e18a44ca3
Kup87 <dami.kuper@gmail.com>
2021-12-09 00:08:52 -0300

    Funca

--- KupEntrenamientosWeb/script.js
+++ KupEntrenamientosWeb/script.js
@@ -17,17 +17,17 @@ document.addEventListener("swiped-right", closeSideNav);
 toggler.addEventListener("click", openSideNav);
 overlay.addEventListener("click", closeSideNav);
 
-
-
 //Activa on scroll view la animación "GaleriaRtoL"
 window.addEventListener('scroll', function(){
     let gal_img = document.getElementsByClassName("gal_img");
-    let marca = document.getElementById("gal_img_animation")
+    let marca = document.getElementById("gal_img_animation");
     let position = marca.getBoundingClientRect().top;
+    console.log(position);
     let windowSize = window.innerHeight;
-
+    console.log(windowSize);
+    
     if (position < windowSize){
         for ( var i = 0; i < gal_img.length; i++){
-        gal_img[i].style.animation = "GaleriaRtoL 3s"
+        gal_img[i].style.animation = "GaleriaRtoL 3s";
     }}
 })
